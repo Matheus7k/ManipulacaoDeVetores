2a741e80c176eccf5e41d9cf60895012f55cf733
Matheus7k <06matheus.oliveira@gmail.com>
2023-03-07 11:54:40 -0300

    Adicionado função de ordenação

--- ManipulacaoDeVetores/Program.cs
+++ ManipulacaoDeVetores/Program.cs
@@ -6,16 +6,20 @@
         int[] vetorB = new int[5];
         int[] vetorC = new int[10];
 
-        populaVetor(vetorA);
-        populaVetor(vetorB);
+        populaVetor(vetorA, "vetor A");
+        populaVetor(vetorB, "vetor B");
 
-        populaVetorC(vetorA, vetorB, vetorC);
+        copiaVetor(vetorA, vetorB, vetorC);
 
-        imprimeVetor(vetorC);
+        imprimeVetor(vetorC, "vetor C");
 
-        void populaVetor(int[] vetor)
+        ordenaVetor(vetorC);
+        imprimeVetor(vetorC, "vetor C ordenado");
+
+
+        void populaVetor(int[] vetor, string texto)
         {
-            Console.WriteLine("Informe 5 valores para o vetor: ");
+            Console.WriteLine($"Informe 5 valores para o {texto}: ");
             for (int i = 0; i < vetor.Length; i++)
             {
                 Console.Write($"{i+1}° posição: ");
@@ -23,7 +27,7 @@
             }
         }
 
-        void populaVetorC(int[] vetorA, int[] vetorB, int[] vetorC)
+        void copiaVetor(int[] vetorA, int[] vetorB, int[] vetorC)
         {
             int i;
             for(i = 0; i < vetorA.Length; i++)
@@ -37,12 +41,29 @@
             }
         }
 
-        void imprimeVetor(int[] vetor)
+        void imprimeVetor(int[] vetor, string texto)
         {
-            Console.Write($"Imprimindo o vetor C: ");
+            Console.Write($"\nImprimindo o {texto}: ");
             foreach (int i in vetor)
             {
                 Console.Write(i + " ");
+            }
+        }
+
+        void ordenaVetor(int[] vetor)
+        {
+            int aux;
+            for(int i = 0; i < vetor.Length; i++)
+            {
+                for(int j = 0; j < vetor.Length; j++)
+                {
+                    if (vetor[i] < vetor[j])
+                    {
+                        aux = vetor[j];
+                        vetor[j] = vetor[i];
+                        vetor[i] = aux;
+                    }
+                }
             }
         }
     }
